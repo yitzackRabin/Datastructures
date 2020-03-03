f38ee36f8ce10ad52020184251bcc6889befe44b
rabin <49115936+yitzackRabin@users.noreply.github.com>
2020-03-03 23:31:41 +0300

    DS continuation

---
+++ src/hashtable/SecondaryHash.java
@@ -0,0 +1,12 @@
+/**
+ * Simple interface file for double hashing
+ *
+ * @author RABIN
+ */
+package hashtable;
+
+public interface SecondaryHash {
+
+  // A method for a second hash function
+  public int hashCode2();
+}
